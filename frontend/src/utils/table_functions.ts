358f447450fd3458c12668c67fbb0da01d4228df
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-08-18 17:02:05 +1200

    implemented table functions

--- frontend/src/utils/table_functions.ts
+++ frontend/src/utils/table_functions.ts
@@ -1,1 +1,21 @@
-//
+/**
+ * Function to sort data based of a sortKey, and whether the sorting should be reversed or not.
+ *
+ * @param tableData the data to sort. This is an array of objects
+ * @sortKey They key to sort by
+ * @param reverse True if we shold reverse the order of sorting (sorts ascending is fale, descending if true)
+ */
+
+export function sortData<T>(
+  tableData: T[],
+  sortKey: keyof T,
+  reverse: boolean
+): T[] {
+  const sortedData = tableData.sort((a, b) => {
+    return a[sortKey] > b[sortKey] ? 1 : -1;
+  });
+  if (reverse) {
+    return sortedData.reverse();
+  }
+  return sortedData;
+}
